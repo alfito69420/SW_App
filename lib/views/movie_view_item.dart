b87b3a0ecffdd433cd96a88a8c71894ac0ec2ae0
Alfredo Aramburo <20031030@itcelaya.edu.mx>
2024-09-18 16:27:34 -0600

    add: drawer

--- lib/views/movie_view_item.dart
+++ lib/views/movie_view_item.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto1/models/movies_dao.dart';
 
-class MovieViewItem extends StatelessWidget {
+class MovieViewItem extends StatefulWidget {
   MovieViewItem({super.key, required this.moviesDao});
 
   MoviesDao moviesDao;
 
   @override
+  State<MovieViewItem> createState() => _MovieViewItemState();
+}
+
+class _MovieViewItemState extends State<MovieViewItem> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text(widget.moviesDao.nameMovie!),
+    );
   }
 }
